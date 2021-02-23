317b0244b3c0877fa0d2a4d1990eb808e57d94ac
Mohamad <mohamadhosseintahan@yahoo.com>
2021-02-24 00:17:14 +0330

    add another counting sort method:

--- data structure - sorts/counting sort.py
+++ data structure - sorts/counting sort.py
@@ -16,3 +16,22 @@ for i in range(m+1):
         A[k], k = i, k+1
 
 print(A)
+# another method for counting sort need python ability
+
+B = [1, 2, 1, 3, 1, 5, 3, 4, 1, 2, 3, 0, 0,
+     3, 0, 1, 0, 1, 2, 3, 2, 5, 4, 5, 4, 1, 0, 3 ,1, 2, 3, 1, 4, 1, 0]
+
+m = max(B)
+n = len(B)
+c = [0]*(m+1)
+print(c)
+for i in B:
+    c[i] += 1
+print(c)
+
+B = []
+
+for i in range(m+1):
+    B += [i] * c[i]
+
+print(B)
